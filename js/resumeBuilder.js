aa6fe3dc976c9965a279db1d13bf881f86a3cc86
Martin Wenzel <martin.wenzel@outlook.com>
2016-06-02 20:40:03 +0200

    Initial resume page complete

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,30 +1,36 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-//  $("#main").append("Martin Wenzel");
-
-//  var AwesomeThoughts;
-//  AwesomeThoughts = "I am Martin and I am awesome";
-//  console.log(AwesomeThoughts);
-
-// var FunThoughts = AwesomeThoughts.replace("awesome","fun");
-// $("#main").append(FunThoughts);
-
 
 var bio = {
-  "name" : "MarTin Wenzel",
+  "name" : "Martin Wenzel",
   "role" : "Web Developer",
-  "welcomeMsg" : "Nerd with business know-how",
-  "pictureURL" : "images/me.jpg",
   "contacts" : {
     "mobile" : "+4917666804004",
     "email" : "martin.wenzel@outlook.com",
     "github" : "smabian",
     "location" : "Stuttgart"
   },
+  "welcomeMessage" : "Project & Account Manager with strong IT know-how",
   "skills" : [
-    "HTML", "CSS", "JavaScript", "Project Management", "PMP", "Sales", "ITIL", "SCRUM"
-  ]
+    "HTML", "CSS", "JavaScript", "Project Management", "PMP", "Sales", "ITIL", "SCRUM", "Account Management"
+  ],
+  "biopic" : "images/me.jpg",
+  display: function (){
+    $("#header").prepend(HTMLheaderRole.replace("%data%", this.role));
+    $("#header").prepend(HTMLheaderName.replace("%data%", this.name));
+    $("#topContacts").append(HTMLmobile.replace("%data%", this.contacts.mobile));
+    $("#topContacts").append(HTMLemail.replace("%data%", this.contacts.email));
+    $("#topContacts").append(HTMLgithub.replace("%data%", this.contacts.github));
+    $("#topContacts").append(HTMLlocation.replace("%data%", this.contacts.location));
+    $("#header").append(HTMLbioPic.replace("%data%", this.biopic));
+    $("#header").append(HTMLwelcomeMsg.replace("%data%", this.welcomeMessage));
+
+    $("#header").append(HTMLskillsStart);
+    for (i=0; i<this.skills.length; i++){
+      $("#skills:last").append(HTMLskills.replace("%data%", this.skills[i]));
+    }
+  }
 }
 
 var work = {
@@ -39,9 +45,16 @@ var work = {
   {
     "employer" : "Hewlett Packard Enterprise",
     "title" : "Cooperative Student",
-    "location" : "Boeblingen, Germany & Palo Alto, California",
+    "location" : "Boeblingen, Germany",
     "dates" : "September 2011 - September 2014",
-    "description" : "Six internship at Hewlett Packard in various busines units and locations worldwide"
+    "description" : "Cooperative Student at Hewlett Packard Enterprise, doing several internships in various businees units"
+  },
+  {
+    "employer" : "Hewlett Packard Enterprise",
+    "title" : "International Internship",
+    "location" : "Palo Alto, California",
+    "dates" : "May 2013 - August 2013",
+    "description" : "Internship in Houston, TX and Palo Alto, CA at Hewlett Packard Enterprise"
   },
   {
     "employer" : "KBF",
@@ -49,17 +62,48 @@ var work = {
     "location" : "Moessingen, Germany",
     "dates" : "July 2010 - July 2011",
     "description" : "KBF is a charitable organization with 74 facilities in 23 locations. The main focus is to provide help for handicapped and elderly persons. This help mostly consists of driving, education and support with food and accommodation."
+  }],
+  display: function (){
+    for (job in this.jobs) {
+      $("#workExperience").append(HTMLworkStart);
+      var formattedEmployer = HTMLworkEmployer.replace("%data%", this.jobs[job].employer);
+      var formattedTitle = HTMLworkTitle.replace("%data%", this.jobs[job].title);
+      var formattedEmployerTitle = formattedEmployer + formattedTitle;
+      $(".work-entry:last").append(formattedEmployerTitle);
+      $(".work-entry:last").append(HTMLworkDates.replace("%data%",this.jobs[job].dates));
+      $(".work-entry:last").append(HTMLworkLocation.replace("%data%",this.jobs[job].location));
+      $(".work-entry:last").append(HTMLworkDescription.replace("%data%",this.jobs[job].description));
+    }
   }
-]}
+}
 
 var projects = {
-  "onlineProjects" : [
+  "projects" : [
   {
     "title" : "Sample Project",
     "dates" : "2016",
     "description" : "Placeholder for project description",
-    "images" : "images/197x148.gif"
-  }]
+    "images" : [
+      "images/197x148.gif", "images/197x148.gif"
+    ]
+  }],
+  display : function(){
+    for(project in this.projects) {
+      $("#projects").append(HTMLprojectStart);
+      var formattedTitle = HTMLprojectTitle.replace("%data%", this.projects[project].title);
+      var formattedDates = HTMLprojectDates.replace("%data%", this.projects[project].dates);
+      var formattedDescription = HTMLprojectDescription.replace("%data%", this.projects[project].description);
+
+      $(".project-entry:last").append(formattedTitle);
+      $(".project-entry:last").append(formattedDates);
+      $(".project-entry:last").append(formattedDescription);
+
+      for(i=0; i < this.projects[project].images.length; i++ ) {
+        var formattedImage = HTMLprojectImage.replace("%data%", this.projects[project].images[i]);
+        $(".project-entry:last").append(formattedImage);
+      }
+    }
+  }
 }
 
 var education = {
@@ -69,98 +113,60 @@ var education = {
     "degree" : "Bachelor of Science",
     "dates" :"September 2011 - September 2014",
     "location" : "Stuttgart, Germany",
-    "major" : ["Business Information Systems"],
+    "majors" : ["Business Management", "Information Technology"],
     "url" : "http://www.dhbw-stuttgart.de/zielgruppen/international-visitors/"
   },{
     "name" : "Technical academic high school",
     "degree" : "A-Level",
     "dates" : "July 2007 - July 2010",
     "location" : "Tuebingen, Germany",
-    "major" : ["Information Technology"],
+    "majors" : ["Information Technology"],
     "url" : "https://www.gs-tuebingen.de/index.php"
   },{
     "name" : "Gorham Fayette High School",
-    "degree" : "",
+    "degree" : "High School Degree",
     "dates" : "July 2006 - June 2007",
-    "location" : "Fayett, OH, USA",
-    "major" : [""],
+    "location" : "Fayette, OH, USA",
+    "majors" : ["General studies"],
     "url" : "http://www.fayettesch.org/"
   }],
   "onlineCourses" : [
   {
     "title" : "Front-End Web Developer Nanodegree",
     "school" : "Udacity",
-    "dates" : "May 2016 - Today",
+    "date" : "May 2016 - Today",
     "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-  }]
-}
+  }],
+  display : function (){
+     for (school in this.schools){
+      $("#education").append(HTMLschoolStart);
+      var formattedSchoolName = HTMLschoolName.replace("%data%",this.schools[school].name);
+      var formattedSchoolNameURL = formattedSchoolName.replace("#",this.schools[school].url);
+      var formattedSchoolDegree = HTMLschoolDegree.replace("%data%",this.schools[school].degree);
+      var formattedSchoolNameDegree = formattedSchoolNameURL + formattedSchoolDegree;
+      $(".education-entry:last").append(formattedSchoolNameDegree);
+      $(".education-entry:last").append(HTMLschoolDates.replace("%data%",this.schools[school].dates));
+      $(".education-entry:last").append(HTMLschoolLocation.replace("%data%",this.schools[school].location));
 
+      for (i=0; i<this.schools[school].majors.length; i++){
+        $(".education-entry:last").append(HTMLschoolMajor.replace("%data%", this.schools[school].majors[i]));
+      }
 
+     }
 
-$("#header").prepend(HTMLheaderRole.replace("%data%", bio.role));
-$("#header").prepend(HTMLheaderName.replace("%data%", bio.name));
-
-$("#topContacts").append(HTMLmobile.replace("%data%", bio.contacts.mobile));
-$("#topContacts").append(HTMLemail.replace("%data%", bio.contacts.email));
-$("#topContacts").append(HTMLgithub.replace("%data%", bio.contacts.github));
-$("#topContacts").append(HTMLlocation.replace("%data%", bio.contacts.location));
-
-$("#header").append(HTMLbioPic.replace("%data%", bio.pictureURL));
-$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg));
-
-
-if (bio.skills.length >= 1) {
-  $("#header").append(HTMLskillsStart);
-  for (i=0; i<bio.skills.length; i++){
-    $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+    //
+    // $(".education-entry").append(HTMLschoolMajor.replace("%data%",education.schoolMajor));
   }
 }
 
-function displaywork(){
-  for (job in work.jobs) {
-    $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
-    $(".work-entry:last").append(HTMLworkDates.replace("%data%",work.jobs[job].dates));
-    $(".work-entry:last").append(HTMLworkLocation.replace("%data%",work.jobs[job].location));
-    $(".work-entry:last").append(HTMLworkDescription.replace("%data%",work.jobs[job].description));
-  }
+function displayMap(){
+  $("#mapDiv").append(googleMap);
 }
 
-// function inName(fullName){
-//   var names = fullName.split(" ");
-//   names[0] = names[0].toLowerCase();
-//   names[0] = names[0][0].toUpperCase() + names[0].slice(1);
-//   names[1] = names[1].toUpperCase();
-//   return names[0] + " " + names [1];
-// }
-// $("#main").append(internationalizeButton);
-
-projects.display = function() {
-  for(project in projects.onlineProjects) {
-    $("#projects").append(HTMLprojectStart);
-    $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.onlineProjects[project].title));
-    $(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.onlineProjects[project].dates));
-    $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.onlineProjects[project].description));
-    $(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.onlineProjects[project].images));
-  }
-}
-
-displaywork();
+bio.display();
+work.display();
 projects.display();
-
-
-// +HTMLworkTitle.replace("%data%",work.workTitle));
-// $(".work-entry").append(HTMLworkDates.replace("%data%",work.workDates));
-// $(".work-entry").append(HTMLworkLocation.replace("%data%",work.workLocation));
-// $(".work-entry").append(HTMLworkDescription.replace("%data%",work.workDescription));
-
-// $("#education").append(HTMLschoolStart);
-// $(".education-entry").append(HTMLschoolName.replace("%data%",education.schoolName)+HTMLschoolDegree.replace("%data%",education.schoolDegree));
-// $(".education-entry").append(HTMLschoolDates.replace("%data%",education.schoolDates));
-// $(".education-entry").append(HTMLschoolLocation.replace("%data%",education.schoolLocation));
-// $(".education-entry").append(HTMLschoolMajor.replace("%data%",education.schoolMajor));
+education.display();
+displayMap();
 
 
